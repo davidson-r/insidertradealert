f0e26d779929e8f3827e5b282af0e7c345563df9
home2418a <davidson.yyz@gmail.com>
2023-04-23 22:36:47 -0400

    files download bash script

---
+++ download_files.sh
@@ -0,0 +1,7 @@
+wget "https://www.sec.gov/Archives/edgar/daily-index/xbrl/companyfacts.zip" -O companyfacts.zip
+unzip companyfacts.zip
+rm companyfacts.zip
+
+wget "https://www.sec.gov/Archives/edgar/daily-index/bulkdata/submissions.zip" -O submissions.zip
+unzip submissions.zip
+rm submissions.zip
