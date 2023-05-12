4ccf47d734ef53780abfbaf5bbb18f7657d7f242
home2418a <davidson.yyz@gmail.com>
2023-05-12 12:58:12 -0400

    use local postgres

--- scripts/conn.py
+++ scripts/conn.py
@@ -2,5 +2,4 @@
 import os
 import sqlalchemy as sa
 
-# engine = sa.create_engine(os.environ.get('DATABASE_URL'))
-engine = sa.create_engine(os.environ.get('postgresql://postgres:localhost:5432/postgres'))
+engine = sa.create_engine(os.environ.get('DATABASE_URL','postgresql://postgres:localhost:5432/postgres'))
