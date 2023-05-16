c0cd9a71d7b540666c9cbe466c9b6aa867e9354d
home2418a <davidson.yyz@gmail.com>
2023-05-16 13:33:22 -0400

    new page

--- download_data.sh
+++ download_data.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cd ~/insidertradealert && curl https://www.sec.gov/Archives/edgar/daily-index/bulkdata/submissions.zip -o submissions.zip -A davidson1987@gmail.com && unzip -q submissions.zip -d submissions && rm -f submissions.zip
+cd ~/insidertradealert && curl https://www.sec.gov/Archives/edgar/daily-index/bulkdata/submissions.zip -o submissions.zip -A davidson1987@gmail.com && unzip -q -o submissions.zip -d submissions && rm -f submissions.zip
 
 
