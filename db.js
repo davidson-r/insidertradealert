d84b9dc48830bb2a809cd721a5a5585d5ee1686f
home2418a <davidson.yyz@gmail.com>
2023-05-20 01:47:36 -0400

    fix db url

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const { Pool } = require("pg");
 
-var port = process.env.NODE_ENV==='development' ? 5433  : 5432
+var port = 5432
+// var port = process.env.NODE_ENV==='development' ? 5433  : 5432
 
 const pool = new Pool({
   connectionString: `postgresql://postgres:postgres@localhost:${port}/postgres`
