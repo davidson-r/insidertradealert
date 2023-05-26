31033de45544bc1698f21c46ef0786737184b596
home2418a <davidson.yyz@gmail.com>
2023-05-26 09:07:00 -0400

    undo index page

--- wait-for-it.sh
+++ wait-for-it.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+# #!/bin/bash
 
-# Wait for the database to be ready
-until pg_isready -h db -p 5432 -q
-do
-    echo "Waiting for the database to be ready..."
-    sleep 2
-done
+# # Wait for the database to be ready
+# until pg_isready -h db -p 5432 -q
+# do
+#     echo "Waiting for the database to be ready..."
+#     sleep 2
+# done
 
-# Run your application build command
-pm2-runtime npm -- start
+# # Run your application build command
+# pm2-runtime npm -- start
