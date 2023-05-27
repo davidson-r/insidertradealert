a91fb3b9831aa3f5b785300469fc49ba19780e48
home2418a <davidson.yyz@gmail.com>
2023-05-26 23:15:33 -0400

    home page

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 
 var connectionstring = process.env.NODE_ENV==='development' ? 
 "postgresql://postgres:postgres@localhost:5433/postgres":
-"postgresql://postgres:postgres@host.docker.internal:5432/postgres"
+"postgresql://postgres:postgres@0.0.0.0:5432/postgres"
 
 
 const pool = new Pool({
