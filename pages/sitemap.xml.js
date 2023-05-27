f32b29abe385230f79684584531d40ad9c5eccf2
home2418a <davidson.yyz@gmail.com>
2023-05-27 19:53:49 -0400

    sitemap

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,31 @@
+function SiteMap() {
+    // getServerSideProps will do the heavy lifting
+}
+
+export async function getServerSideProps({ res }) {
+
+    res.setHeader('Content-Type', 'text/xml');
+    res.write(`
+  <?xml version="1.0" encoding="UTF-8"?>
+    <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        <sitemap>
+            <loc>http://insidertradealert.com/sitemap1.xml</loc>
+        </sitemap>
+        <sitemap>
+            <loc>http://insidertradealert.com/sitemap2.xml</loc>
+        </sitemap>
+        <sitemap>
+            <loc>http://insidertradealert.com/sitemap3.xml</loc>
+        </sitemap>
+        <sitemap>
+            <loc>http://insidertradealert.com/sitemap4.xml</loc>
+        </sitemap>
+    </sitemapindex>`);
+    res.end();
+
+    return {
+        props: {},
+    };
+}
+
+export default SiteMap;
---
+++ pages/sitemap3.xml.js
@@ -0,0 +1,60 @@
+const DOMAIN_URL = 'http://insidertradealert.com';
+const slugify = require('../utils/functions');
+import pool from '../db';
+
+function generateSiteMap(items) {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+     <url>
+       <loc>http://insidertradealert.com</loc>
+       <changefreq>daily</changefreq>
+        <priority>1</priority>
+
+     </url>
+
+     ${items
+       .map(item => {
+         return `
+       <url>
+           <loc>${`${DOMAIN_URL}/${item.entity_type}/${slugify(item.entity_name)}-${item.cik}`}</loc>
+            <changefreq>weekly</changefreq>
+            <priority>0.5</priority>
+       </url>
+     `;
+       })
+       .join('')}
+   </urlset>
+ `;
+}
+
+function SiteMap() {
+  // getServerSideProps will do the heavy lifting
+}
+
+export async function getServerSideProps({ res }) {
+
+  var sitemap_query = await pool.query(`
+    select distinct on(cik)* from (
+        select distinct report_owner_cik cik, report_owner_name entity_name,'reporter' entity_type
+            from submissions where report_owner_name is not null
+        )t
+        union select distinct on(cik)* from (
+            select distinct issuer_cik cik, issuer_name entity_name,'issuer'entity_type
+            from submissions where issuer_name is not null
+        )t
+        order by 1,2
+        offset 100000
+        limit 150000
+        ;`)
+  const sitemap = generateSiteMap(sitemap_query.rows);
+
+  res.setHeader('Content-Type', 'text/xml');
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+}
+
+export default SiteMap;
