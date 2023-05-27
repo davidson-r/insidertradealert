0d65c52102eaaebb2cd5518505871d81138a1bc8
home2418a <davidson.yyz@gmail.com>
2023-05-27 16:46:07 -0400

    https

---
+++ generate_sitemap.js
@@ -0,0 +1,15 @@
+
+
+var urls = [{
+    "loc":"http://insidertradealert.com/",
+    "lastModified":"http://insidertradealert.com/",
+    "changefreq":"weekly",
+    "priority":"weekly",
+},{
+    "loc":"http://insidertradealert.com/",
+    "lastModified":"http://insidertradealert.com/",
+    "changefreq":"weekly",
+    "priority":"1.0",
+},
+
+]
