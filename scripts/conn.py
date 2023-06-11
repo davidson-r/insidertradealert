fe439c1cb715576333f46cc4f4e128cc651f23a3
home2418a <davidson.yyz@gmail.com>
2023-06-11 15:11:59 -0400

    docker cron env

--- scripts/conn.py
+++ scripts/conn.py
@@ -4,7 +4,7 @@ import sqlalchemy as sa
 import platform
 
 
-DATABASE_URL="postgresql://postgres:postgres@localhost:5433/postgres" if platform.system()=='Darwin' else "postgresql://postgres:postgres@localhost:5432/postgres"
+DATABASE_URL="postgresql://postgres:postgres@localhost:5433/postgres" if platform.system()=='Darwin' else "postgresql://postgres:postgres@172.19.0.1:5432/postgres"
 
 
-engine = sa.create_engine(os.environ.get('DATABASE_URL',DATABASE_URL))
+engine = sa.create_engine(DATABASE_URL)
